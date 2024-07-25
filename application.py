939e1f3f65dad49358d20700ee17bd11ec868ef0
Bizbalt <yannik.koester@uni-jena.de>
2024-07-25 16:25:24 +0200

    :construction: embed dash

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ from flask import Flask
 from views import views
 
 # instantiate the app
+# ToDo: I love this post on how to embed Dash in Flask: https://hackersandslackers.com/plotly-dash-with-flask/
 app = Flask(__name__)
 
 app.register_blueprint(views, url_prefix='/')
